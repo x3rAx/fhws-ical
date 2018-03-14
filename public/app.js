e72d1406c3c8a46810165ee64290062e746f1f8e
Björn Richter <x3ro1989@gmail.com>
2018-03-14 15:38:32 +0100

    Fix indentation

--- public/app.js
+++ public/app.js
@@ -1,28 +1,28 @@
 new Vue({
-  el: '#app',
-  data: {
-    target: 'https://fiw.fhws.de/fileadmin/share/vlplan/iCal/BaInf1_2017ws.ics',
-    url: ''
-  },
-  created: function() {
-    this.updateUrl();
-  },
-  watch: {
-    target: function() {
-      this.updateUrl();
-    }
-  },
-  methods: {
-    updateUrl: function() {
-      if (!/^http[s]*:\/\/fiw\.fhws\.de\/fileadmin\/share\/vlplan\/iCal\/[^\/]+.ics$/.test(this.target)) {
-        this.url = '';
-      } else {
-        // Get filename
-        var filename = this.target.split('/').splice(-1)[0];
+    el: '#app',
+    data: {
+        target: 'https://fiw.fhws.de/fileadmin/share/vlplan/iCal/BaInf1_2017ws.ics',
+        url: ''
+    },
+    created: function() {
+        this.updateUrl();
+    },
+    watch: {
+        target: function() {
+            this.updateUrl();
+        }
+    },
+    methods: {
+        updateUrl: function() {
+            if (!/^http[s]*:\/\/fiw\.fhws\.de\/fileadmin\/share\/vlplan\/iCal\/[^\/]+.ics$/.test(this.target)) {
+                this.url = '';
+            } else {
+                // Get filename
+                var filename = this.target.split('/').splice(-1)[0];
 
-        this.url = 'http://fhws-ical.x3ro.net/' + filename;
-      }
+                this.url = 'http://fhws-ical.x3ro.net/' + filename;
+            }
+        }
     }
-  }
 });
 
